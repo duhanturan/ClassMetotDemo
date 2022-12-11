ea305f73a589b3856cf3da08da8b64eb69d2087f
DUHAN-TURAN <dduhanturann@gmail.com>
2022-12-11 22:06:57 +0300

    11.12.2022 Çalışma

---
+++ Collections/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Ahmet", "Habib", "Omer", "Fatih" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+
+            //isimler = new string[5];
+            //isimler[4] = "Mehmet";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[4]);
+
+            List<string> isimler = new List<string> { "Ahmet", "Habib", "Omer", "Fatih" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+            isimler.Add("Duhan");
+            Console.WriteLine(isimler[4]);
+            Console.WriteLine(isimler[0]);
+        }
+    }
+}
