7c08247476126e716ab4800436b06cbf95ea0b52
DUHAN-TURAN <dduhanturann@gmail.com>
2022-12-12 08:45:16 +0300

    12.12.2022 MyDictionary TempArray orneği

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary<T>
+    {
+        T[] items ;
+        public MyDictionary()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] TempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < TempArray.Length; i++)
+            {
+                items[i] = TempArray[i];
+            }
+            items[items.Length - 1] = item;
+
+        }
+    }
+}
